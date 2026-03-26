9d197ad68f47aa34b14413f9992fb119156d569d
adamwillerton <131967086+adamwillerton@users.noreply.github.com>
2026-03-26 09:50:13 +0000

    Update api.spec.ts

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -5,3 +5,18 @@ test("GET /health returns ok", async ({ request }) => {
   expect(res.status()).toBe(200);
   expect(await res.json()).toEqual({ status: "ok" });
 });
+
+
+test("GET /staff returns first page of staff", async ({ request }) => {
+  const res = await request.get("http://localhost:8000/staff");
+  expect(res.status()).toBe(200);
+
+  const data = await res.json();
+
+  expect(data.total).toBe(10);          // you have 10 staff in DB
+  expect(data.page).toBe(1);
+  expect(data.page_size).toBe(10);
+  expect(data.items.length).toBe(10);
+});
+
+
